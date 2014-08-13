555b9383b99a228c16591ca209e5b7bb45f5358c
calvached <calvache.diana@gmail.com>
2014-08-13 11:48:09 -0500

    Creating different size boards

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,35 @@
 class Board
-  attr_reader :gameboard
+  attr_accessor :gameboard
+  attr_reader :io
 
-  def initialize
-    @gameboard = {
-      '1' => '', '2' => '', '3' => '',
-      '4' => '', '5' => '', '6' => '',
-      '7' => '', '8' => '', '9' => '' }
+  MESSAGES = {
+    'invalid' => 'Invalid selection. Please try again.'
+    }
+
+  def initialize(grid_size, input_output)
+    @io = input_output
+    @grid_size = grid_size
+    @gameboard = {}
+  end
+
+  def display
+    # need to puts onto the screen
+    @gameboard
+  end
+
+  def create
+    (grid_spaces).each { |space| @gameboard[space] = '' }
+  end
+
+  def grid_spaces
+    (1..(@grid_size**2))
   end
 
   def place_game_piece(position, game_piece)
     if @gameboard[position].empty?
       fill_space(position, game_piece)
     else
-      puts 'Invalid selection. Please try again'
+      @io.out(MESSAGES['invalid'])
     end
   end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,36 @@
 require 'board'
+require 'input_output'
 
 describe Board do
-  let (:board) { Board.new }
+  let (:board) { Board.new(3, MockIO.new) }
 
-  it 'fills in a space on the board' do
-    board.fill_space('4', 'x')
+  it 'displays the board' do
+    board.gameboard = {
+      '1' => 'x', '2' => '', '3' => 'x',
+      '4' => '', '5' => 'o', '6' => '',
+      '7' => '', '8' => 'o', '9' => ''
+    }
 
-    expect(board.gameboard['4']).to eq('x')
+    expect(board.display).to eq({
+      "1"=>"x", "2"=>"", "3"=>"x",
+      "4"=>"", "5"=>"o", "6"=>"",
+      "7"=>"", "8"=>"o", "9"=>""
+    })
   end
 
   it 'fills space in available positions only' do
-    board.place_game_piece('4', 'x')
-    board.place_game_piece('4', 'o')
+    board.create
+    board.place_game_piece(4, 'x')
+    board.place_game_piece(4, 'o')
 
-    expect(board.gameboard['4']).to eq('x')
+    expect(board.gameboard[4]).to eq('x')
+  end
+
+  it 'responds with a message to the user if a space is taken' do
+    board.create
+    board.place_game_piece(4, 'x')
+    board.place_game_piece(4, 'o')
+
+    expect(board.io.messages).to eq(["Invalid selection. Please try again."])
   end
 end
