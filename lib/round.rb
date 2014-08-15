36b91c5ea687e08da6422193e893a053bacbd16f
calvached <calvache.diana@gmail.com>
2014-08-15 10:58:39 -0500

    Round selects an o and x player

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,51 @@
 class Round
-  attr_reader :message
+  attr_reader :message, :o_player, :x_player
 
-  def initialize(board, ui_messages, input_output)
+  def initialize(board, ui_messages, input_output, ai, player)
     @board = board
     @message = ui_messages
     @io = input_output
+    @ai = ai
+    @player = player
+    @o_player = nil
+    @x_player = nil
   end
 
   def start_game
     @message.welcome
     @message.player_options
 
+    user_input = @io.in
+
+    case user_input
+    when '1'
+      @message.how_to_play
+    when '2'
+      play
+    when '3'
+      @message.player_options
+    else
+      @message.invalid_option
+    end
+  end
+
+  def play
+    @board.create
+    random_select_players
+    assign_game_pieces
+
+    p @board.gameboard
+    @board.place_game_piece(1, 'x')
+    p @board.gameboard
+  end
+
+  def random_select_players
+    @o_player, @x_player = [@ai, @player].shuffle
+  end
+
+  def assign_game_pieces
+    @o_player.game_piece = 'O'
+    @x_player.game_piece = 'X'
   end
 end
 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,10 +2,25 @@ require 'round'
 require 'board'
 require 'input_output'
 require 'messages'
+require 'ai'
+require 'player'
 
 describe Round do
   let(:mock) { MockIO.new }
   let(:ui_messages) { Messages.new(mock) }
-  let (:round) { Round.new(Board.new(3, mock), ui_messages, mock) }
+  let (:round) { Round.new(Board.new(3, mock), ui_messages, mock, AI.new, Player.new) }
 
+  it "selects an 'O' player" do
+    round.random_select_players
+    round.assign_game_pieces
+
+    expect(round.o_player.game_piece).to eq('O')
+  end
+
+  it "selects an 'X' player" do
+    round.random_select_players
+    round.assign_game_pieces
+
+    expect(round.x_player.game_piece).to eq('X')
+  end
 end
