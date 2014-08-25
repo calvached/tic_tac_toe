286fe9538c0d9380193a85cb363cfe2f1c1c6fbc
calvached <calvache.diana@gmail.com>
2014-08-25 15:58:24 -0500

    Adding notes for integration test

---
+++ spec/integration_spec.rb
@@ -0,0 +1,53 @@
+#it 'creates an AI player' do
+#    round.io.inputs = ['2', '3']
+#
+#    require 'pry'
+#    binding.pry
+#
+#    round.start_game
+#
+#    expect(round.ai).to be_instance_of(AI)
+#  end
+#
+#  it 'creates a Human player' do
+#    round.io.inputs = ['2', '3', '3']
+#    round.start_game
+#
+#    expect(round.human).to be_instance_of(Human)
+#  end
+#
+#  it "displays 'Welcome'" do
+#    round.io.inputs = ['1', '2', '3']
+#    round.start_game
+#
+#    expect(round.io.received_messages).to include(Messages::WELCOME)
+#  end
+#
+#  it "displays 'How to Play' instructions" do
+#    round.io.inputs = ['1', '2', '3']
+#    round.start_game
+#
+#    expect(round.io.received_messages).to include(Messages::HOW_TO_PLAY)
+#  end
+#
+#  it "displays options for the player to choose from" do
+#    round.io.inputs = ['3', '2', '3']
+#    round.start_game
+#
+#    expect(round.io.received_messages).to include(Messages::PLAYER_OPTIONS)
+#  end
+#
+#  it "displays an 'invalid selection' message if option not available" do
+#    round.io.inputs = ['hey', '2', '3']
+#    round.start_game
+#
+#    expect(round.io.received_messages).to include(Messages::INVALID_OPTION)
+#  end
+#
+#  it 'asks gameboard size input' do
+#    round.io.inputs = ['2', '3', '3']
+#    round.start_game
+#
+#    expect(round.io.received_messages).to include("Please input the gameboard size (e.g. '3' for 3x3): ")
+#  end
+#
