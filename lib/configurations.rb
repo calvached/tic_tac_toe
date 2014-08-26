cac47a4120b5ea2c6c0a59f2f7305f5abc4d7506
calvached <calvache.diana@gmail.com>
2014-08-26 15:30:47 -0500

    Shuffling players and creating board

--- lib/configurations.rb
+++ lib/configurations.rb
@@ -1,11 +1,47 @@
+require 'messages'
+require 'board'
+require 'human'
+require 'ai'
+
 class Configurations
-  def setup
+  attr_reader :io
+
+  def initialize(input_output)
+    @board = Board.new
+    @io = input_output
   end
-  def play
+
+  def setup
+    create_board
+    # figure out a way to have player choose human v human, ai v human, ai v ai
+    shuffle_player_order(AI.new('X'), Human.new('O', @io))
+
+    settings = { player_one: @player_one, player_two: @player_two, board: @board }
+  end
+
+  private
+  def create_board
     @io.out(Messages::ASK_FOR_BOARD_SIZE)
-    create_players if get_board_size
+    get_board_size
+  end
+
+  def shuffle_player_order(contestant_1, contestant_2)
+    @player_one, @player_two = [contestant_1, contestant_2].shuffle
+  end
+
+  def get_board_size
+    board_size = @io.input
+
+    if board_size =~ /\d/
+      @board.create(board_size.to_i)
+    else
+      @io.out(Messages::INVALID_CHARACTER)
+      get_board_size
+    end
   end
 end
+
+# Config = [Human, AI, Board]
 
 #Round only cares about Human, AI and Board
 #Configuration does the rest of the set up (board size, ai difficulty, player names, etc)
--- spec/configurations_spec.rb
+++ spec/configurations_spec.rb
@@ -1,4 +1,28 @@
-#require 'config'
-#
-#describe Config do
-#end
+require 'configurations'
+require 'input_output'
+
+describe Configurations do
+  let(:config) { Configurations.new(MockIO.new) }
+
+  before(:example) do
+    allow(config.io).to receive(:input).and_return('3')
+  end
+
+  it 'creates game_settings' do
+    expect(config.setup).to be_kind_of(Hash)
+    expect(config.setup.length).to eq(3)
+  end
+
+  it 'creates Player One' do
+    expect(config.setup[:player_one]).to be_a_kind_of(Object)
+  end
+
+  it 'creates Player Two' do
+    expect(config.setup[:player_two]).to be_a_kind_of(Object)
+  end
+
+  it 'creates a Board' do
+    expect(config.setup[:board]).to be_an_instance_of(Board)
+  end
+
+end
