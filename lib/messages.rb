7cc860aa9876dee6d109ccaac459cf6f6ff56767
calvached <calvache.diana@gmail.com>
2014-08-28 16:27:05 -0500

    Adding new messages

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,26 +1,29 @@
 class Messages
 
-  WELCOME = '###########################\n' +
-    '#                         #\n' +
-    '# Welcome to Tic Tac Toe! #\n' +
-    '#                         #\n' +
-    '###########################\n'
+  WELCOME = "###########################\n" +
+    "#                         #\n" +
+    "# Welcome to Tic Tac Toe! #\n" +
+    "#                         #\n" +
+    "###########################\n\n"
 
-  PLAYER_OPTIONS = 'Please make a selection from the following: \n' +
-    '1. How to Play\n' +
-    '2. Play Game\n' +
-    '3. Help\n'
+  PLAYER_OPTIONS = "\nPlease make a selection from the following: \n" +
+    "1. How to Play\n" +
+    "2. Play Game\n" +
+    "3. Help\n" +
+    "4. Quit\n"
 
-  HOW_TO_PLAY = '###############\n' +
-    '# How to Play #\n' +
-    '###############\n' +
+  HOW_TO_PLAY = "\n###############\n" +
+    "# How to Play #\n" +
+    "###############\n" +
     "Each player is assigned a game piece of either 'O' or 'X'\n" +
     "after which each will take turns placing a piece on the board.\n" +
     "The objective of the game is to fill in three of a kind in\n" +
     "a row, column, or diagonal. The first to accomplish this wins the game.\n" +
     "If neither player is declared a winner then the game ends in a draw.\n"
 
-  INVALID_OPTION = 'Invalid selection. Please try again or press 3 for help.'
+  INVALID_OPTION = "Invalid selection. Please try again or press 3 for help."
+
+  QUIT = 'Goodbye...'
 
   INVALID_RESPONSE = 'Please input a valid response.'
 
