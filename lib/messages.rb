87e70491434d3ddd1b796ebfc6777dcd2ef9ba2f
calvached <calvache.diana@gmail.com>
2014-09-02 22:12:51 -0500

    Adding new messags for UI

--- lib/messages.rb
+++ lib/messages.rb
@@ -9,19 +9,18 @@ class Messages
   PLAYER_OPTIONS = "\nPlease make a selection from the following: \n" +
     "1. How to Play\n" +
     "2. Play Game\n" +
-    "3. Help\n" +
-    "4. Quit\n"
+    "3. Quit\n"
 
   HOW_TO_PLAY = "\n###############\n" +
     "# How to Play #\n" +
     "###############\n" +
-    "Each player is assigned a game piece of either 'O' or 'X'\n" +
-    "after which each will take turns placing a piece on the board.\n" +
+    "Each player is assigned a game piece after which\n" +
+    "each will take turns placing a piece on the board.\n" +
     "The objective of the game is to fill in three of a kind in\n" +
     "a row, column, or diagonal. The first to accomplish this wins the game.\n" +
     "If neither player is declared a winner then the game ends in a draw.\n"
 
-  INVALID_OPTION = "Invalid selection. Please try again or press 3 for help."
+  INVALID_OPTION = "Invalid selection."
 
   QUIT = 'Goodbye...'
 
@@ -31,9 +30,25 @@ class Messages
 
   MAKE_MOVE = "Please select a cell on the board: "
 
+  INVALID_MOVE= "That space is already taken. Please choose again."
+
   ASK_FOR_GAME_TYPE = "Would you like to play against a human or AI? Enter 'H' for Human and 'A' for AI"
+
+  ASK_FOR_NAME = 'What is your name? (Use only letters and numbers)'
+
+  ASK_FOR_GAMEPIECE = "Please choose a game piece from the following: 'O, $, %, &, *, @, #, ?'"
+
+  DRAW = "DRAW!"
 
   def self.prettify_board(row)
     row.join(' | ') + "\n" + "____" * row.length
+  end
+
+  def self.print_header(player)
+    "#{player.name.capitalize}'s Turn. Your game piece is: #{player.game_piece}"
+  end
+
+  def self.print_round_win(winner)
+    "#{winner.name.capitalize} has won!!!"
   end
 end
