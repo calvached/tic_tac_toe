743eca99b989e86da27e98681877096137957b38
calvached <calvache.diana@gmail.com>
2014-09-04 15:23:42 -0500

    Working on integration tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,3 +1,37 @@
+require 'round'
+require 'configurations'
+require 'input_output'
+require 'ai'
+require 'human'
+require 'board'
+require 'messages'
+
+describe Round do
+  let (:mock) { MockIO.new }
+  let (:board) { Board.new }
+  let (:round) { Round.new(Configurations.new(mock), mock) }
+  let (:human) { Human.new('O', 'diana', mock) }
+  let (:ai) { AI.new('X') }
+  let (:game_settings) {{ player_one: human,
+                          player_two: ai,
+                          board: board }}
+
+  it 'plays the game' do
+    board.gameboard = {
+        '1' => 'X', '2' => 'O', '3' => ' ',
+        '4' => 'O', '5' => ' ', '6' => ' ',
+        '7' => 'X', '8' => 'O', '9' => 'X'
+      }
+
+    allow(round.configurations).to receive(:setup).and_return(game_settings)
+
+    round.io.inputs = ['2', '5', '3']
+    round.start_game
+
+    expect(round.io.received_messages).to include(Messages.print_round_win(human))
+  end
+end
+
 #it 'creates an AI player' do
 #    round.io.inputs = ['2', '3']
 #
