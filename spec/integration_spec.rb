85dc51cefd8f3ae565fae8d918df0193007a8420
calvached <calvache.diana@gmail.com>
2014-09-08 11:25:53 -0500

    Adding tests for draw game, human win and invalid selections

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -19,7 +19,7 @@ describe Round do
                           player_two: ai,
                           board: board }}
 
-  it 'plays the game' do
+  it 'plays the game and displays the winner' do
     board.gameboard = {
         '1' => 'X', '2' => 'O', '3' => ' ',
         '4' => 'O', '5' => ' ', '6' => ' ',
@@ -33,58 +33,49 @@ describe Round do
 
     expect(round.io.received_messages).to include(Messages.print_round_win(human))
   end
-end
 
-#it 'creates an AI player' do
-#    round.io.inputs = ['2', '3']
-#
-#    require 'pry'
-#    binding.pry
-#
-#    round.start_game
-#
-#    expect(round.ai).to be_instance_of(AI)
-#  end
-#
-#  it 'creates a Human player' do
-#    round.io.inputs = ['2', '3', '3']
-#    round.start_game
-#
-#    expect(round.human).to be_instance_of(Human)
-#  end
-#
-#  it "displays 'Welcome'" do
-#    round.io.inputs = ['1', '2', '3']
-#    round.start_game
-#
-#    expect(round.io.received_messages).to include(Messages::WELCOME)
-#  end
-#
-#  it "displays 'How to Play' instructions" do
-#    round.io.inputs = ['1', '2', '3']
-#    round.start_game
-#
-#    expect(round.io.received_messages).to include(Messages::HOW_TO_PLAY)
-#  end
-#
-#  it "displays options for the player to choose from" do
-#    round.io.inputs = ['3', '2', '3']
-#    round.start_game
-#
-#    expect(round.io.received_messages).to include(Messages::PLAYER_OPTIONS)
-#  end
-#
-#  it "displays an 'invalid selection' message if option not available" do
-#    round.io.inputs = ['hey', '2', '3']
-#    round.start_game
-#
-#    expect(round.io.received_messages).to include(Messages::INVALID_OPTION)
-#  end
-#
-#  it 'asks gameboard size input' do
-#    round.io.inputs = ['2', '3', '3']
-#    round.start_game
-#
-#    expect(round.io.received_messages).to include("Please input the gameboard size (e.g. '3' for 3x3): ")
-#  end
-#
+  it "displays a 'How to Play' message if player selects the option" do
+    board.gameboard = {
+        '1' => 'X', '2' => 'O', '3' => ' ',
+        '4' => 'O', '5' => ' ', '6' => ' ',
+        '7' => 'X', '8' => 'O', '9' => 'X'
+      }
+
+    allow(round.configurations).to receive(:setup).and_return(game_settings)
+
+    round.io.inputs = ['1', '2', '5', '3']
+    round.start_game
+
+    expect(round.io.received_messages).to include(Messages::HOW_TO_PLAY)
+  end
+
+  it "displays an 'Invalid Option' message if player selects an unavailable option" do
+    board.gameboard = {
+        '1' => 'X', '2' => 'O', '3' => ' ',
+        '4' => 'O', '5' => ' ', '6' => ' ',
+        '7' => 'X', '8' => 'O', '9' => 'X'
+      }
+
+    allow(round.configurations).to receive(:setup).and_return(game_settings)
+
+    round.io.inputs = ['hey', '2', '5', '3']
+    round.start_game
+
+    expect(round.io.received_messages).to include(Messages::INVALID_OPTION)
+  end
+
+  it "plays the game and displays 'Draw' if tie game" do
+    board.gameboard = {
+        '1' => 'X', '2' => 'O', '3' => 'X',
+        '4' => 'X', '5' => 'O', '6' => ' ',
+        '7' => 'O', '8' => 'X', '9' => 'X'
+      }
+
+    allow(round.configurations).to receive(:setup).and_return(game_settings)
+
+    round.io.inputs = ['1', '2', '6', '3']
+    round.start_game
+
+    expect(round.io.received_messages).to include(Messages::DRAW)
+  end
+end
